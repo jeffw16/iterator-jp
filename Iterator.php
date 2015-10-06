b62d2f0f90eeb2f40661cacd6b0c108d32494b42
Jeffrey Wang <jeffw16@users.noreply.github.com>
2015-10-06 08:59:42 -0500

    Update Iterator.php

--- Iterator.php
+++ Iterator.php
@@ -6,36 +6,40 @@
 */
 
 public class Iterator {
-  public $arr;
-  public $index = 0;
-  function __construct( $in_arr ) {
-    $arr = $in_arr;
-  }
-  function hasNext() {
-    if ( sizeof ( $arr ) >= $index ) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-  function next() {
-    return $arr[$index++];
-  }
-  function remove() {
-    $arr_temp = $arr;
-    unset( $arr_temp[$index] );
-    for ( $i = $index; $i < sizeof( $arr ); $i++ ) {
-      $arr_temp[$i] = $arr_temp[$i+1];
-    }
-    $arr = $arr_temp;
-    return $arr[$index];
-  }
-  function previous() {
-    return $arr[$index--];
-  }
-  function set( $in_set ) {
-    $set_old = $arr[$index];
-    $arr[$index] = $in_set;
-    return $set_old;
-  }
+	public $arr;
+	public $index = 0;
+	
+	function __construct( $in_arr ) {
+	  $arr = $in_arr;
+	}
+	function iterator( $in_arr ) {
+	 $arr = $in_arr;
+	}
+	function hasNext() {
+	  if ( sizeof ( $arr ) >= $index ) {
+	    return false;
+	  } else {
+	    return true;
+	  }
+	}
+	function next() {
+	  return $arr[$index++];
+	}
+	function remove() {
+	  $arr_temp = $arr;
+	  unset( $arr_temp[$index] );
+	  for ( $i = $index; $i < sizeof( $arr ); $i++ ) {
+	    $arr_temp[$i] = $arr_temp[$i+1];
+	  }
+	  $arr = $arr_temp;
+	  return $arr[$index];
+	}
+	function previous() {
+	  return $arr[$index--];
+	}
+	function set( $in_set ) {
+	  $set_old = $arr[$index];
+	  $arr[$index] = $in_set;
+	  return $set_old;
+	}
 }
